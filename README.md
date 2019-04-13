50ad7b392a856264d65c2d8fc3f88fc794fbdfbc
hans <laderud@hotmail.com>
2019-04-13 22:57:22 +0200

    utlilze config

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # StatusBear
 # Go code for talking with [i3 bar](https://i3wm.org/i3bar/) using [i3 protocol](https://i3wm.org/docs/i3bar-protocol.html) and creating usefull and fast bars for multiple monitors.  
-# Very much WIP.
+# WIP.
+
+
+# Example usage (in swaywm)
+```
+bar {
+    position bottom
+    status_command go run ~/go/src/github.com/afreakk/i3statusbear/main.go ~/go/src/github.com/afreakk/i3statusbear/configExample.json
+	output $primaryScreen
+    colors {
+        statusline #ffffff
+        background #323232
+        inactive_workspace #32323200 #32323200 #5c5c5c
+    }
+}
+```
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"os"
 	"syscall"
 	"time"
@@ -12,6 +12,25 @@ import (
 	"github.com/robfig/cron"
 	"github.com/sqp/pulseaudio"
 )
+
+type Module struct {
+	Color          string `json:"color,omitempty"`
+	Background     string `json:"background,omitempty"`
+	Border         string `json:"border,omitempty"`
+	MinWidth       int    `json:"min_width,omitempty"`
+	Align          string `json:"align,omitempty"`
+	Urgent         bool   `json:"urgent,omitempty"`
+	Separator      bool   `json:"separator,omitempty"`
+	SeparatorWidth int    `json:"separator_block_width,omitempty"`
+	Markup         string `json:"markup,omitempty"`
+
+	Name     string `json:"name,omitempty"`
+	Instance string `json:"instance,omitempty"`
+}
+
+type Config struct {
+	Modules []Module `json:"modules"`
+}
 
 // === START: helper types for decode and encode to i3bar protocol ===
 type Protocol struct {
@@ -35,51 +54,146 @@ type Click struct {
 }
 
 type Message struct {
-	Position       int    `json:"-"`
 	FullText       string `json:"full_text"`
 	ShortText      string `json:"short_text,omitempty"`
 	Color          string `json:"color,omitempty"`
 	Background     string `json:"background,omitempty"`
 	Border         string `json:"border,omitempty"`
-	MinWidth       string `json:"min_width,omitempty"`
-	Align          Align  `json:"align,omitempty"`
+	MinWidth       int    `json:"min_width,omitempty"`
+	Align          string `json:"align,omitempty"`
 	Name           string `json:"name,omitempty"`
 	Instance       string `json:"instance,omitempty"`
 	Urgent         bool   `json:"urgent,omitempty"`
 	Separator      bool   `json:"separator,omitempty"`
 	SeparatorWidth int    `json:"separator_block_width,omitempty"`
+	Markup         string `json:"markup,omitempty"`
 }
-
-type Align string
-
-const (
-	LEFT   Align = "left"
-	RIGHT  Align = "right"
-	CENTER Align = "center"
-)
 
 // === END: helper types for decode and encode to i3bar protocol ===
 
-func handleInput(logger *log.Logger) {
+func handleInput() {
 	decodeStdin := json.NewDecoder(os.Stdin)
 	// Read openbracket
 	_, err := decodeStdin.Token()
 	if err != nil {
-		logger.Fatal(err)
+		panic(err)
 	}
 	for decodeStdin.More() {
 		click := &Click{}
 		err := decodeStdin.Decode(click)
 		if err != nil {
-			logger.Fatal(err)
+			panic(err)
 		}
-		logger.Println(click)
+		fmt.Println(json.Marshal(click))
 	}
 }
 
-func handleOutput(logger *log.Logger) {
+func applyModuleConfigToMessage(module Module, message *Message) {
+	if module.Align != "" {
+		message.Align = module.Align
+	}
+	if module.Background != "" {
+		message.Background = module.Background
+	}
+	if module.Border != "" {
+		message.Border = module.Border
+	}
+	if module.Color != "" {
+		message.Color = module.Color
+	}
+	if module.MinWidth != 0 {
+		message.MinWidth = module.MinWidth
+	}
+	if module.Separator != false {
+		message.Separator = module.Separator
+	}
+	if module.SeparatorWidth != 0 {
+		message.SeparatorWidth = module.SeparatorWidth
+	}
+	if module.Urgent != false {
+		message.Urgent = module.Urgent
+	}
+	if module.Markup != "" {
+		message.Markup = module.Markup
+	}
+
+	if module.Name != "" {
+		message.Name = module.Name
+	}
+	if module.Instance != "" {
+		message.Instance = module.Instance
+	}
+}
+
+func module_datetime(output *Output, module Module) {
+
+	// cron can be used by many modules
+	c := cron.New()
+	// === Start: DateTime setup ===
+	formatDateTimeMsg := func() string {
+		return time.Now().Format("15:04 1/Jan")
+	}
+	dateTimeMsg := &Message{
+		FullText: formatDateTimeMsg(),
+	}
+	applyModuleConfigToMessage(module, dateTimeMsg)
+	c.AddFunc("0 * * * * *", func() {
+		dateTimeMsg.FullText = formatDateTimeMsg()
+		output.printMsgs()
+	})
+	c.Start()
+	// === End: DateTime setup ===
+	output.messages = append(output.messages, dateTimeMsg)
+}
+
+func module_pulseaudio(output *Output, module Module) error {
+	// === Start: PulseAudio setup ===
+	pulse, e := pulseaudio.New()
+	if e != nil {
+		panic(e)
+	}
+
+	var pathToFallbackSink dbus.ObjectPath
+	// Here we assume you are using fallbacksink, so we query that
+	// altough later in pulse-callback we render whatever device you changed volume on :)
+	pulse.Core().Get("FallbackSink", &pathToFallbackSink)
+	volumes, e := pulse.Device(pathToFallbackSink).ListUint32("Volume")
+	if e != nil {
+		return e
+	}
+	baseVolume, e := pulse.Device(pathToFallbackSink).Uint32("BaseVolume")
+	if e != nil {
+		return e
+	}
+	formatPulseAudioText := func(volumes []uint32, baseVolume uint32) string {
+		return fmt.Sprintf("vol %.2f", float32(volumes[0])/float32(baseVolume))
+	}
+	pulseAudioMsg := &Message{
+		FullText: formatPulseAudioText(volumes, baseVolume),
+	}
+	applyModuleConfigToMessage(module, pulseAudioMsg)
+	updatePulseMsg := func(volumes []uint32, baseVolume uint32) {
+		pulseAudioMsg.FullText = formatPulseAudioText(volumes, baseVolume)
+		output.printMsgs()
+	}
+
+	client := &Client{pulse, updatePulseMsg}
+	pulse.Register(client)
+
+	go pulse.Listen()
+	// === End: PulseAudio setup ===
+	output.messages = append(output.messages, pulseAudioMsg)
+	return nil
+}
+
+type Output struct {
+	encodeToStdout *json.Encoder
+	messages       []*Message
+}
+
+func (o *Output) init() {
 	// init stdout writer
-	encodeToStdout := json.NewEncoder(os.Stdout)
+	o.encodeToStdout = json.NewEncoder(os.Stdout)
 
 	// then we need to specify protocol we use
 	protocol := Protocol{
@@ -90,102 +204,62 @@ func handleOutput(logger *log.Logger) {
 		ClickEvents: true,
 	}
 	// and send it to Stdout
-	encodeToStdout.Encode(protocol)
-
-	messages := []*Message{}
-
+	o.encodeToStdout.Encode(protocol)
 	// start our array of arrays.. (we never end it though)
 	// kindof hacky, is there a better way ?
 	fmt.Print("[")
-	// render method.. gets called by modules when they need to redraw
-	renderMsgs := func() {
-		encodeToStdout.Encode(messages)
-		// And then separator between messages in our infinite array that never ends
-		fmt.Print(",")
-	}
 
-	// cron can be used by many modules
-	c := cron.New()
+	o.messages = []*Message{}
+}
 
-	// === Start: DateTime setup ===
-	formatDateTimeMsg := func() string {
-		return time.Now().Format("15:04 1/Jan")
-	}
-	dateTimeMsg := &Message{
-		FullText: formatDateTimeMsg(),
-		Name:     "dateTimeMsg",
-	}
-	c.AddFunc("0 * * * * *", func() {
-		dateTimeMsg.FullText = formatDateTimeMsg()
-		renderMsgs()
-	})
-	// === End: DateTime setup ===
-
-	// === Start: PulseAudio setup ===
-	pulse, e := pulseaudio.New()
-	if e != nil {
-		logger.Panicln(e)
-	}
-
-	var pathToFallbackSink dbus.ObjectPath
-	// Here we assume you are using fallbacksink, so we query that
-	// altough later in pulse-callback we render whatever device you changed volume on :)
-	pulse.Core().Get("FallbackSink", &pathToFallbackSink)
-	volumes, e := pulse.Device(pathToFallbackSink).ListUint32("Volume")
-	if e != nil {
-		logger.Panicln(e)
-	}
-	baseVolume, e := pulse.Device(pathToFallbackSink).Uint32("BaseVolume")
-	if e != nil {
-		logger.Panicln(e)
-	}
-	formatPulseAudioText := func(volumes []uint32, baseVolume uint32) string {
-		return fmt.Sprintf("vol %.2f", float32(volumes[0])/float32(baseVolume))
-	}
-	pulseAudioMsg := &Message{
-		FullText: formatPulseAudioText(volumes, baseVolume),
-		Name:     "pulseMsg",
-	}
-	updatePulseMsg := func(volumes []uint32, baseVolume uint32) {
-		pulseAudioMsg.FullText = formatPulseAudioText(volumes, baseVolume)
-		renderMsgs()
-	}
-
-	client := &Client{logger, pulse, updatePulseMsg}
-	pulse.Register(client)
-
-	go pulse.Listen()
-	// === End: PulseAudio setup ===
-
-	//ordering of modules
-	messages = append(messages, pulseAudioMsg)
-	messages = append(messages, dateTimeMsg)
-
-	c.Start()
-	renderMsgs()
+func (o Output) printMsgs() {
+	o.encodeToStdout.Encode(o.messages)
+	// And then separator between messages in our infinite array that never ends
+	fmt.Print(",")
 }
 
 func main() {
 	// Init log etc
-	logFile, _ := os.OpenFile("log/i3statusbear"+time.Now().Format(time.RFC3339)+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	logger := log.New(logFile, "logger: ", log.Lshortfile)
+	configFilePath := os.Args[1]
+	configFile, error := os.Open(configFilePath)
+	if error != nil {
+		panic(error)
+	}
+	defer configFile.Close()
+	configTxt, error := ioutil.ReadAll(configFile)
+	if error != nil {
+		panic(error)
+	}
+	var config Config
+	error = json.Unmarshal(configTxt, &config)
+	if error != nil {
+		panic(error)
+	}
 
-	logger.Println("starting..")
-	go handleInput(logger)
-	handleOutput(logger)
+	go handleInput()
+
+	output := Output{}
+	output.init()
+	for _, module := range config.Modules {
+		switch module.Name {
+		case "datetime":
+			module_datetime(&output, module)
+		case "pulseaudio":
+			module_pulseaudio(&output, module)
+		}
+	}
+	output.printMsgs()
 	//hacky way of blocking forever..
 	select {}
 }
 
 // Pulse audio subscribe stuff
 type Client struct {
-	logger         *log.Logger
 	pulse          *pulseaudio.Client
 	updatePulseMsg func(volumes []uint32, baseVolume uint32)
 }
 
 func (cl *Client) DeviceVolumeUpdated(path dbus.ObjectPath, values []uint32) {
-	cl.logger.Println("device volume", path, values)
 	var baseVolume uint32
 	cl.pulse.Device(path).Get("BaseVolume", &baseVolume)
 	cl.updatePulseMsg(values, baseVolume)
