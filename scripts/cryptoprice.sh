5ce993f36caac5dec0a8ba8b74bb58451023c2f5
hans <laderud@hotmail.com>
2019-06-27 21:20:49 +0200

    script and config improvements

--- scripts/cryptoprice.sh
+++ scripts/cryptoprice.sh
@@ -2,4 +2,4 @@
 USD_PRICE=$(curl https://api.coinmarketcap.com/v1/ticker/$1/ 2> /dev/null | jq -r '.[0] | .price_usd')
 #we only want first 5 difits, any more is overkill
 #and use sed to remove last character if it is a decimal separator
-echo ${USD_PRICE:0:5} | sed 's/\.$//'
+echo $USD_PRICE | awk -F. '{print $1}'
