ae0344c4a3d97f40554480dd5efca2aa2904b779
hans <laderud@hotmail.com>
2019-10-29 19:40:25 +0100

    safer bar rendering

--- internal/util/util.go
+++ internal/util/util.go
@@ -44,11 +44,16 @@ func ApplyModuleConfigToMessage(module config.Module, message *protocol.Message)
 		message.Instance = module.Instance
 	}
 }
-
 func RenderBar(module config.Module, filled int64, total int64) string {
 	filledBarsCount := filled * module.BarWidth / total
-	remainingCount := module.BarWidth - filledBarsCount
+	remainingCount := Max(module.BarWidth-filledBarsCount, 0)
 	filledBars := strings.Repeat(module.BarFilled, int(filledBarsCount))
 	emptyBars := strings.Repeat(module.BarEmpty, int(remainingCount))
 	return fmt.Sprintf(module.Sprintf, filledBars, emptyBars)
+}
+func Max(x, y int64) int64 {
+	if x > y {
+		return x
+	}
+	return y
 }
